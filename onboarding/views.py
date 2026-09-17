cb247172e639bcbf40c495a60b7ab81e1c541d88
Alex van der Leeuw <alleeuw@gmail.com>
2026-09-17 10:52:58 +0200

    Set up base views and made them work with the InBrowser API view to verify the API functions.

--- onboarding/views.py
+++ onboarding/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from onboarding.models import Category, Expense, Income
+from onboarding.serializers import CategorySerializer, ExpenseSerializer, IncomeSerializer
 
-# Create your views here.
+class CategoryList(ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class CategoryDetails(RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class ExpenseList(ListCreateAPIView):
+    queryset = Expense.objects.all()
+    serializer_class = ExpenseSerializer
+
+class ExpenseDetals(RetrieveUpdateDestroyAPIView):
+    queryset = Expense.objects.all()
+    serializer_class = ExpenseSerializer
+
+class IncomeList(ListCreateAPIView):
+    queryset = Income.objects.all()
+    serializer_class = IncomeSerializer
+
+class IncomeDetails(RetrieveUpdateDestroyAPIView):
+    queryset = Income.objects.all()
+    serializer_class = IncomeSerializer
