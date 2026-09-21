64eb20e3007d5c8d3000bfb5773f6a29cb24c95b
Alex van der Leeuw <alleeuw@gmail.com>
2026-09-21 14:35:34 +0200

    Token authorisation implementation

--- web_project/settings.py
+++ web_project/settings.py
@@ -132,6 +132,6 @@ MAILERS = {
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 5,
-    #'DEFAULT_AUTHENTICATION_CLASSES' : ['rest_framework.authentication.TokenAuthentication'],
-    #'DEFAULT_PERMISSION_CLASSES' : ['rest_framework.permissions.IsAuthenticated'],
+    'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework.authentication.TokenAuthentication'],
+    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticated'],
 }
