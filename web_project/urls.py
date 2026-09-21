b4ee14bcbe422157d9cf1d0cf685a5af29de41fa
Alex van der Leeuw <alleeuw@gmail.com>
2026-09-21 15:34:09 +0200

    Implemented error views for the most likely to be relevant errors

--- web_project/urls.py
+++ web_project/urls.py
@@ -1,23 +1,30 @@
-"""
-URL configuration for web_project project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('onboarding.urls')),
-]
+﻿"""
+URL configuration for web_project project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/6.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from onboarding import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('onboarding.urls')),
+]
+
+handler400 = views.error_400
+handler403 = views.error_403
+handler404 = views.error_404
+handler405 = views.error_405
+handler500 = views.error_500
