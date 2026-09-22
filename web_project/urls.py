9c4dad15a0993b65950a33f55bc80f393a2c5908
Alex van der Leeuw <alleeuw@gmail.com>
2026-09-22 14:37:18 +0200

    Implemented commons/utils into the project to reuse repeated functions instead of remaking them completely each time

--- web_project/urls.py
+++ web_project/urls.py
@@ -14,13 +14,24 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve as serve_static
 from finance_manager.views import error_400, error_403, error_404, error_405, error_500
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('finance_manager.urls')),
+]
+
+# DEBUG is False even in local development for this project (that's what makes
+# the custom handler400/403/404/405/500 views below fire instead of Django's
+# own debug pages), so runserver's automatic static handler never kicks in.
+# Serve the collected static/ files (run `manage.py collectstatic` after
+# changing anything under a static/ folder) explicitly instead.
+urlpatterns += [
+    re_path(r'^%s(?P<path>.*)$' % settings.STATIC_URL.lstrip('/'), serve_static, {'document_root': settings.STATIC_ROOT}),
 ]
 
 handler400 = error_400
