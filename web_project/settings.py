abcf760fd1fe0a8fe70211a4cfa5690d5aecd5e0
Alex van der Leeuw <alleeuw@gmail.com>
2026-09-24 09:35:31 +0200

    Updated back to simpler version of the static url, and swapped back to DEBUG=True to make it work
    Custom error pages won't get shown for now, but this is only an issue for local development as static files are handled differently in actual launches.

--- web_project/settings.py
+++ web_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--)^n8cqs(*4wb037+#16q-(^$&zy974i!j88!t*+3z+^-_h-gk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
 
--- web_project/urls.py
+++ web_project/urls.py
@@ -1,23 +1,7 @@
-﻿"""
-URL configuration for web_project project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.conf import settings
+﻿from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include, re_path
-from django.views.static import serve as serve_static
+from django.urls import path, include
+from django.conf.urls.static import static
 from finance_manager.views import error_400, error_403, error_404, error_405, error_500
 
 urlpatterns = [
@@ -25,14 +9,7 @@ urlpatterns = [
     path('', include('finance_manager.urls')),
 ]
 
-# DEBUG is False even in local development for this project (that's what makes
-# the custom handler400/403/404/405/500 views below fire instead of Django's
-# own debug pages), so runserver's automatic static handler never kicks in.
-# Serve the collected static/ files (run `manage.py collectstatic` after
-# changing anything under a static/ folder) explicitly instead.
-urlpatterns += [
-    re_path(r'^%s(?P<path>.*)$' % settings.STATIC_URL.lstrip('/'), serve_static, {'document_root': settings.STATIC_ROOT}),
-]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 handler400 = error_400
 handler403 = error_403
